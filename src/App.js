191f1e31a3ba80dacd7954a9512964d27be92493
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-01-18 23:48:35 +0900

    router test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 import New from './pages/New';
+
+import RouteTest from './components/RouteTest';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <h1>app.js</h1>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/new' element={<New />}></Route>
+          <Route path='/edit' element={<Edit />}></Route>
+          <Route path='/diary' element={<Diary />}></Route>
+        </Routes>
+        <RouteTest />
       </div>
     </BrowserRouter>
   );
