839c8691b294c412a72c983671b54e04a4ec7a0f
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-01-19 01:02:38 +0900

    프로젝트 기초공사1_폰트,레이아웃,이미지,공통컴포넌트(버튼,헤더,스타일)

--- src/App.js
+++ src/App.js
@@ -6,20 +6,57 @@ import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 import New from './pages/New';
 
-import RouteTest from './components/RouteTest';
+//Components
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
 
 function App() {
+
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
   return (
     <BrowserRouter>
       <div className="App">
+
+        <MyHeader headtext={"App"}
+          leftChild={
+            <MyButton text={'왼쪽버튼'} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton text={'오른쪽버튼'} onClick={() => alert("오른쪽 클릭")} />
+          }
+        />
+
         <h1>app.js</h1>
+
+        <img src={process.env.PUBLIC_URL + '/assets/emotion1.png'}></img>
+        <img src={process.env.PUBLIC_URL + '/assets/emotion2.png'}></img>
+        <img src={process.env.PUBLIC_URL + '/assets/emotion3.png'}></img>
+        <img src={process.env.PUBLIC_URL + '/assets/emotion4.png'}></img>
+        <img src={process.env.PUBLIC_URL + '/assets/emotion5.png'}></img>
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼")}
+          type={"positive"}></MyButton>
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼")}
+          type={"nagative"}></MyButton>
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼")}
+          type={"default"}></MyButton>
+
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/new' element={<New />}></Route>
           <Route path='/edit' element={<Edit />}></Route>
           <Route path='/diary/:id' element={<Diary />}></Route>
         </Routes>
-        <RouteTest />
+
       </div>
     </BrowserRouter>
   );
