f3b76f317fac9eb88ee107bcb56746e5ae379355
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-01-18 23:32:01 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Diary from './pages/Diary';
+import Edit from './pages/Edit';
+import New from './pages/New';
 
 function App() {
   return (
-    <div className="App">
-      <h1>helloddddd</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>app.js</h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
