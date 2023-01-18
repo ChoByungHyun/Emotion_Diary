5a9b0ac63e084ce64c292e3485d11afe45aac43b
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-01-19 00:09:09 +0900

    navigate

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,10 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
+
+    const navigate = useNavigate();
 
     const id = searchParams.get("id");
     const mode = searchParams.get("mode")
@@ -14,6 +16,8 @@ const Edit = () => {
         <h1>Edit</h1>
         <p>이곳은 Edit 입니다.</p>
         <button onClick={() => setSearchParams({ who: "cbh" })}>QS바꾸기</button>
+        <button onClick={() => navigate("/home")}>홈으로 가기</button>
+        <button onClick={() => navigate(-1)}>뒤로 가기</button>
     </div>
 }
 export default Edit;
