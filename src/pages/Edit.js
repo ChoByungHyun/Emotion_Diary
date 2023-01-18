be87ac143d2ad390467631a969912ff30d51be9d
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-01-19 00:04:53 +0900

    Path Variable. Query String

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,19 @@
+import { useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const id = searchParams.get("id");
+    const mode = searchParams.get("mode")
+
+    console.log(id)
+    console.log(mode)
+
     return <div>
         <h1>Edit</h1>
         <p>이곳은 Edit 입니다.</p>
+        <button onClick={() => setSearchParams({ who: "cbh" })}>QS바꾸기</button>
     </div>
 }
 export default Edit;
